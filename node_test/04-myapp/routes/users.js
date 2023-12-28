be2cc2be861806e6f2a1fe2f22bd903107e259a5
chenchao <1366294101@qq.com>
2023-12-28 23:08:43 +0800

    mongodb create

--- node_test/04-myapp/routes/users.js
+++ node_test/04-myapp/routes/users.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require('../model/UserModal');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log(req.cookies)
-  res.cookie('age', 'male')
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    console.log(req.cookies)
+    res.cookie('age', 'male')
+    res.send('respond with a resource');
+});
+
+router.post('/user/add', (req, res) => {
+    // console.log(req.body)
+    // 1.创建一个模型
+    // user.create user.find user.update user.delete
+    let {username, password, age} = req.body;
+    UserModel.create({
+        username, password, age
+    }).then(data => console.log(data))
+    res.send({
+        ok: 1,
+    });
 });
 
 module.exports = router;
