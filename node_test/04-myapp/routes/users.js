7cc16ecb5d8bddf20f679d4f527a45a43421ff7a
chenchao <1366294101@qq.com>
2023-12-29 21:54:01 +0800

    restful

--- node_test/04-myapp/routes/users.js
+++ node_test/04-myapp/routes/users.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.post('/user/add', (req, res) => {
+router.post('/user', (req, res) => {
     // console.log(req.body)
     // 1.创建一个模型
     // user.create user.find user.update user.delete
@@ -22,7 +22,7 @@ router.post('/user/add', (req, res) => {
     });
 });
 
-router.put('/user/update/:id', (req, res) => {
+router.put('/user/:id', (req, res) => {
     // console.log(req.body, req.params)
     let {username, password, age} = req.body;
     UserModel.updateMany({
@@ -35,14 +35,14 @@ router.put('/user/update/:id', (req, res) => {
     });
 });
 
-router.delete('/user/delete/:id', (req, res) => {
+router.delete('/user/:id', (req, res) => {
     UserModel.deleteOne({_id: req.params.id}).then(data => console.log(data))
     res.send({
         ok: 1
     });
 });
 
-router.get('/user/list', (req, res) => {
+router.get('/user', (req, res) => {
     console.log(UserModel.schema.obj)
     UserModel.find({}, ['username', 'password', 'age', '_id'])
         .sort({age: 1})
