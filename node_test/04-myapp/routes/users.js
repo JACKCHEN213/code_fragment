203004b4f7571b2cb30150b3bb2a0e40bb374d0e
chenchao <1366294101@qq.com>
2023-12-29 21:48:48 +0800

    mongodb create update delete find

--- node_test/04-myapp/routes/users.js
+++ node_test/04-myapp/routes/users.js
@@ -22,4 +22,33 @@ router.post('/user/add', (req, res) => {
     });
 });
 
+router.put('/user/update/:id', (req, res) => {
+    // console.log(req.body, req.params)
+    let {username, password, age} = req.body;
+    UserModel.updateMany({
+        _id: req.params.id
+    }, {
+        username, password, age
+    }).then(data => console.log(data))
+    res.send({
+        ok: 1
+    });
+});
+
+router.delete('/user/delete/:id', (req, res) => {
+    UserModel.deleteOne({_id: req.params.id}).then(data => console.log(data))
+    res.send({
+        ok: 1
+    });
+});
+
+router.get('/user/list', (req, res) => {
+    console.log(UserModel.schema.obj)
+    UserModel.find({}, ['username', 'password', 'age', '_id'])
+        .sort({age: 1})
+        .then(data => {
+            res.send(data);
+        })
+});
+
 module.exports = router;
